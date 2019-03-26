19cc8a052189d4d37742101ea55b1e26483b65e9
Tiago Faria <tiagohpf@github.com>
2019-03-26 11:18:21 +0000

    Little changes on microservices

--- microservices/app.js
+++ microservices/app.js
@@ -3,32 +3,32 @@ let axios = require('axios');
 let bodyParser = require('body-parser');
 let url = require('url');
 let Promise = require('es6-promise').Promise;
-
 let request = require('request');
-let Requests = require('./requests');
+
 let DB_PREFIX = 'http://10.113.141.31:8080/transcriptions/';
 let DB_DIALOGUES = DB_PREFIX + 'dialogues';
 let DB_ENTITIES = DB_PREFIX + 'entities';
 let ENTITIES_SERVICE = "http://10.113.134.43:4567/getEntities/";
 let INTENTS_SERVICE = 'http://10.113.141.31:8900/sofia/question';
+let TRANSCRIPT_SERVICE = 'http://10.113.155.13:5500/';
 
 let app = express();
 
 app.use(bodyParser.json());
 app.set('json spaces', 4);
 
-var server = app.listen(3500, function () {
-    console.log("Server listen on 3500")
+let server = app.listen(3500, function () {
+    console.log("Server listen on 3500");
 });
 
-server.setTimeout(250000);
+server.setTimeout(300000);
 
 app.get("/getTranscriptions", (req, res) => {
     axios.get(DB_DIALOGUES)
-        .then(function (response) {
+        .then(response => {
             res.send(response.data._embedded);
         })
-        .catch(function (error) {
+        .catch(error => {
             res.send(error);
         });
 });
@@ -36,10 +36,10 @@ app.get("/getTranscriptions", (req, res) => {
 app.get("/getTranscription/:id", (req, res) => {
     let id = req.params['id'];
     axios.get(DB_DIALOGUES + "/" + id)
-        .then(function (response) {
+        .then(response => {
             res.send(response.data);
         })
-        .catch(function (error) {
+        .catch(error => {
             res.send(error);
         });
 });
@@ -58,60 +58,46 @@ app.get("/transcript", (req, res) => {
         speed = 1.0;
 
     let params = {
-        "path": path,
-        "volume": volume,
-        "speed": speed
+        path: path,
+        volume: volume,
+        speed: speed
     };
 
-    request({ url: 'http://localhost:5000/transcript/', qs: params }, function (err, response, body) {
+    request({url: TRANSCRIPT_SERVICE + 'transcript/', qs: params}, function (err, response, body) {
         res.send(body);
     });
 });
 
 app.get("/getEntities", (req, res) => {
     axios.get(DB_ENTITIES)
-        .then(function (response) {
+        .then(response => {
             res.send(response.data._embedded);
         })
-        .catch(function (error) {
+        .catch(error => {
             res.send(error);
         })
 });
 
 app.get("/getEntities/:sentence", (req, res) => {
-    var sentence = req.params['sentence'];
-    axios.get("http://10.113.134.43:4567/getEntities/" + sentence)
-        .then(function (response) {
-            res.send(response.data.data)
+    let sentence = req.params['sentence'];
+    getEntities(sentence)
+        .then(response => {
+            res.send(response.data);
         })
-        .catch(function (error) {
+        .catch(error => {
             res.send(error);
-        });
+        })
 });
 
 app.get("/getIntent/:sentence", (req, res) => {
     let sentence = req.params['sentence'];
-    axios({
-        method: 'post',
-        url: INTENTS_SERVICE,
-        headers: {
-            'cache-control': 'no-cache',
-            'content-type': 'application/json',
-            'postman-token': 'f66f3311-d2ad-5ea5-5f83-7ad26df1d1d9',
-        },
-        data: {
-            question: sentence,
-            sessionId: 'teste1',
-            VA: 'Telco',
-            isTest: true
-        }
-    })
-        .then(function (response) {
-            res.send(response.data.nlpResponse.intent);
+    getIntent(sentence)
+        .then(response => {
+            res.send(response);
         })
-        .catch(function (error) {
+        .catch(error => {
             res.send(error);
-        });
+        })
 });
 
 app.get("/generateTranscription", (req, res) => {
@@ -128,61 +114,56 @@ app.get("/generateTranscription", (req, res) => {
         speed = 1.0;
 
     let params = {
-        "path": path,
-        "volume": volume,
-        "speed": speed
+        path: path,
+        volume: volume,
+        speed: speed
     };
 
-    request({ url: 'http://localhost:5000/generateTranscription/', qs: params }, function (err, response, body) {
+    request({url: TRANSCRIPT_SERVICE + 'generateTranscription/', qs: params}, function (err, response, body) {
         res.send(body);
     });
 });
 
 app.get("/generateEntities/:file_id", (req, res) => {
-    return new Promise((resolve, reject) => {
-        let id = req.params['file_id'];
-        let promises = [];
-        let dialogues = [];
-        let entities = [];
+    let id = req.params['file_id'];
+    let promises = [];
+    let dialogues = [];
+    let entities = [];
 
-        axios.get(DB_DIALOGUES + "/" + id).then(response => {
-            var data = response.data;
+    axios.get(DB_DIALOGUES + "/" + id).then( response => {
+        let data = response.data;
 
-            data.dialogues.forEach(dialogue => {
-                let p = getEntities(dialogue.text).then(result => {
-                    let json_obj = {
-                        "index": dialogue.index,
-                        "speaker": dialogue.speaker,
-                        "text": dialogue.text,
-                        "entities": result.data
-                    };
-                    if (dialogue.intents)
-                        json_obj["intents"] = dialogue.intents
-                    entities.push.apply(entities, result.data);
-                    dialogues.push(json_obj);
-                    return {
-                        "entities": entities,
-                        "dialogues": dialogues
-                    };
-                });
-                promises.push(p)
+        data.dialogues.forEach(dialogue => {
+            let p = getEntities(dialogue.text).then(result => {
+                let json_obj = {
+                    index: dialogue.index,
+                    speaker: dialogue.speaker,
+                    text: dialogue.text,
+                    entities: result.data
+                };
+
+                if (dialogue.intents)
+                    json_obj.intents = dialogue.intents;
+
+                entities.push.apply(entities, result.data);
+                dialogues.push(json_obj);
+                return { entities, dialogues };
             });
-
-            Promise.all(promises).then(response => {
-                var obj = response[0];
-                axios.patch(DB_DIALOGUES + "/" + id, {
-                    _id: obj.id,
-                    entities: obj["entities"],
-                    dialogues: obj["dialogues"]
-                }).then(r => {
-                    res.send(JSON.stringify(r.data));
-                });
-            });
-
+            promises.push(p)
         });
-        /*.catch(error => {
-            res.send(error);
-        })*/
+
+        return Promise.all(promises).then(response => {
+            let obj = response[0];
+            return axios.patch(DB_DIALOGUES + "/" + id, {
+                _id: obj.id,
+                entities: obj.entities,
+                dialogues: obj.dialogues
+            }).then(r => {
+                res.send(JSON.stringify(r.data));
+            });
+        });
+    }).catch(error => {
+        throw error;
     });
 });
 
@@ -222,22 +203,19 @@ app.get("/generateIntents/:file_id", (req, res) => {
                 res.send(JSON.stringify(r.data));
             });
         });
-
+    }).catch(error => {
+        throw error;
     });
-    /*.catch(error => {
-        res.send(error);
-    })*/
 });
 
+
 function getEntities(sentence) {
-    return new Promise((resolve, reject) => {
-        axios.get(encodeURI(ENTITIES_SERVICE + sentence)).then(response => {
-            resolve(response.data);
+    return axios.get(encodeURI(ENTITIES_SERVICE + sentence)).then( response => {
+            return response.data;
         }).catch(error => {
-            reject(error);
+            throw error;
         });
-    });
-};
+}
 
 function getIntent(sentence) {
     return axios({
